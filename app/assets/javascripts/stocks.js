e997c9637cc21b27d455dac742da761797e84043
RamyElFarou <ramyelfarou@gmail.com>
2016-08-19 02:17:03 +0000

    add user and stock association

--- app/assets/javascripts/stocks.js
+++ app/assets/javascripts/stocks.js
@@ -1,27 +1,25 @@
 var init_stock_lookup;
 
 init_stock_lookup = function() {
-  $('#stock-lookup-form').on('ajax:before', function(event, data, status){ //show while searching
+  $('#stock-lookup-form').on('ajax:before', function(event, data, status){
     show_spinner();
   });
-
-  $('#stock-lookup-form').on('ajax:after', function(event, data, status){ //hide when search is completed
+  
+  $('#stock-lookup-form').on('ajax:after', function(event, data, status){
     hide_spinner();
   });
-
+  
   $('#stock-lookup-form').on('ajax:success', function(event, data, status){
-    $("#stock-lookup").replaceWith(data);
-    init_stock_lookup(); // we need this because listeners are gone when we replace with data
+    $('#stock-lookup').replaceWith(data);
+    init_stock_lookup();
   });
-
-  $('#stock-lookup-form').on('ajax:error', function(event, xhr, status, error) {
-    $('#stock-lookup-results').replaceWith(' ');
-    $('#stock-lookup-errors').replaceWith('Stock was not found');
+  
+  $('#stock-lookup-form').on('ajax:error', function(event, xhr, status, error){
     hide_spinner();
+    $('#stock-lookup-results').replaceWith(' ');
+    $('#stock-lookup-errors').replaceWith('Stock was not found.');
   });
 }
-
-
 
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
+
+
   devise_for :users
+  resources :user_stocks, except: [:show, :edit, :update]
+  resources :users, only: [:show]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
    root 'welcome#index'
-  get 'my_portfolio', to: 'users#my_portfolio'
-  get 'search_stocks', to: "stocks#search"
+   get 'my_portfolio', to: 'users#my_portfolio'
+   get 'search_stocks', to: "stocks#search"
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
